303aaa8b1e73187eb33009dedae11fc36b61953a
rizkiadp <sukmahawaiza@gmail.com>
2025-06-10 20:29:01 +0700

    add perulanan setiap baris

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -31,4 +31,18 @@ int main() {
     // menunjuk ke sebuah file
     infile.open(NamaFile, ios::in);
     cout << endl << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+
+    
 }
