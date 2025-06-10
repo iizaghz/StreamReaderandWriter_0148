020d4f985efd166c50c42cac14be487b1ad72b9f
rizkiadp <sukmahawaiza@gmail.com>
2025-06-10 20:34:04 +0700

    add try untuk melemparkan sebuah integer

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
-
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 5; // mellemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    
+}
