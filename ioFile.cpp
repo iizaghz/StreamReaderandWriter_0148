a02678d63fa64c01f065855dc42668db3eb6a8df
rizkiadp <sukmahawaiza@gmail.com>
2025-06-10 19:29:52 +0700

    add infile untuk menunjuk ke sebuah file

--- ioFile.cpp
+++ ioFile.cpp
@@ -27,5 +27,9 @@ int main()
     outfile.close();
     // menulis file dalam mode membaca
     ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+    cout << endl
+         << ">= Membuka dan mebaca file " << endl;
     
 }
